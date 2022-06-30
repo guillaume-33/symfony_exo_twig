563123b7f60d5dfbffa7d87228130a5bbb4538ac
guillaume <laloum.guillaume@gmail.com>
2022-06-30 15:53:23 +0200

    ajout fichier twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,16 +3,23 @@
 namespace App\Controller;
 
 use phpDocumentor\Reflection\Types\AbstractList;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class MainController extends AbstractList
+class MainController extends AbstractController
 {
   /**
    * @Route("/accueil" , name="accueil")
    */
 
   public function accueil(){
-      return new Response("hello ! ");
+
+      $name= 'Guillaume'; //créé la variable 'name'
+      return $this-> render("accueil.html.twig", ['name' => $name]);
+      // permet de renvoyer la ou les variables vers la page
+      // "acceuil.html.twig
   }
+
+
 }
