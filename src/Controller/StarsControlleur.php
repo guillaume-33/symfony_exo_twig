99f744fc2824654bc63a6e9292bce4cd3ff54e7f
guillaume <laloum.guillaume@gmail.com>
2022-07-01 16:01:55 +0200

    exo tableau star avec include et lien

--- src/Controller/StarsControlleur.php
+++ src/Controller/StarsControlleur.php
@@ -18,7 +18,8 @@ class StarsControlleur extends AbstractController
                     'color' => 'red',
                     'published' => true,
                     'image' => 'https://resize.programme-television.ladmedia.fr/r/670,670/img/var/premiere/storage/images/tele-7-jours/news-tv/video-les-z-amours-france-2-tex-craque-sa-chemise-devant-les-candidats-4344637/77880010-1-fre-FR/VIDEO-Les-Z-amours-France-2-Tex-craque-sa-chemise-devant-les-candidats.jpg',
-                    'publishedDate' => new \DateTime('NOW')
+                    'publishedDate' => new \DateTime('NOW'),
+                    'id'=>1
                 ],
                 2 => [
                     'title' => 'Cyril Hanouna',
@@ -26,7 +27,8 @@ class StarsControlleur extends AbstractController
                     'color' => 'yellow',
                     'published' => true,
                     'image' => 'https://media.onvoitout.fr/2021/10/cyril-hanouna-tpmp-stresse-tres-inquiet-pour-les-yeux-de-benjamin-castaldi-il-dit-tout-50.jpg',
-                    'publishedDate' => new \DateTime('NOW')
+                    'publishedDate' => new \DateTime('NOW'),
+                    'id'=>2
                 ],
                 3 => [
                     'title' => 'Afida Turner',
@@ -34,23 +36,27 @@ class StarsControlleur extends AbstractController
                     'color' => 'black',
                     'published' => true,
                     'image' => 'https://s.yimg.com/uu/api/res/1.2/SXrg2uonJIrD9JlemslJWQ--~B/aD01Njg7dz0xMDI0O2FwcGlkPXl0YWNoeW9u/http://media.zenfs.com/fr_FR/News/TeleLoisirs/96851-afida-turner-atteinte-de-deux-tumeurs-elle-sera-bientot-hospitalisee.jpeg',
-                    'publishedDate' => new \DateTime('NOW')
+                    'publishedDate' => new \DateTime('NOW'),
+                    'id'=>3
+
                 ],
                 4 => [
                     'title' => 'Manuel Valls',
                     'description' => 'Eric',
                     'color' => 'blue',
-                    'published' => false,
+                    'published' => true,
                     'image' => 'https://images.bfmtv.com/vypLwZsN4IyBgQiA4BpWRJgRrpg=/4x3:1252x705/1248x0/images/-80516.jpg',
-                    'publishedDate' => new \DateTime('NOW')
+                    'publishedDate' => new \DateTime('NOW'),
+                    'id'=>4
                 ],
                 5 => [
                     'title' => 'JCVD',
                     'description' => 'Eric',
                     'color' => 'blue',
-                    'published' => false,
+                    'published' => true,
                     'image' => 'https://www.programme.tv/imgre/fit/~1~tel~2021~07~12~6801abcb-554d-4a8b-8667-d05cb7f88734.png/1160x500/crop-from/top/quality/80/focus-point/1783,871/jean-claude-van-damme-devoile-les-causes-de-ses-problemes-de-drogue.jpg',
-                    'publishedDate' => new \DateTime('NOW')
+                    'publishedDate' => new \DateTime('NOW'),
+                    'id'=>5
                 ]
             ];
 
@@ -72,7 +78,8 @@ class StarsControlleur extends AbstractController
                         'color' => 'red',
                         'published' =>true,
                         'image' => 'https://resize.programme-television.ladmedia.fr/r/670,670/img/var/premiere/storage/images/tele-7-jours/news-tv/video-les-z-amours-france-2-tex-craque-sa-chemise-devant-les-candidats-4344637/77880010-1-fre-FR/VIDEO-Les-Z-amours-France-2-Tex-craque-sa-chemise-devant-les-candidats.jpg',
-                        'publishedDate' => new \DateTime('NOW')
+                        'publishedDate' => new \DateTime('NOW'),
+                        'id'=>1
                     ],
                     2 => [
                         'title' => 'Cyril Hanouna',
@@ -80,7 +87,8 @@ class StarsControlleur extends AbstractController
                         'color' => 'yellow',
                         'published' => true,
                         'image' => 'https://media.onvoitout.fr/2021/10/cyril-hanouna-tpmp-stresse-tres-inquiet-pour-les-yeux-de-benjamin-castaldi-il-dit-tout-50.jpg',
-                        'publishedDate' => new \DateTime('NOW')
+                        'publishedDate' => new \DateTime('NOW'),
+                        'id'=>2
                     ],
                     3 => [
                         'title' => 'Afida Turner',
@@ -88,23 +96,26 @@ class StarsControlleur extends AbstractController
                         'color' => 'black',
                         'published' => true,
                         'image' => 'https://s.yimg.com/uu/api/res/1.2/SXrg2uonJIrD9JlemslJWQ--~B/aD01Njg7dz0xMDI0O2FwcGlkPXl0YWNoeW9u/http://media.zenfs.com/fr_FR/News/TeleLoisirs/96851-afida-turner-atteinte-de-deux-tumeurs-elle-sera-bientot-hospitalisee.jpeg',
-                        'publishedDate' => new \DateTime('NOW')
+                        'publishedDate' => new \DateTime('NOW'),
+                        'id'=>3
                     ],
                     4 => [
                         'title' => 'Manuel Valls',
                         'description' => 'Eric',
                         'color' => 'blue',
-                        'published' => false,
+                        'published' => true,
                         'image' => 'https://images.bfmtv.com/vypLwZsN4IyBgQiA4BpWRJgRrpg=/4x3:1252x705/1248x0/images/-80516.jpg',
-                        'publishedDate' => new \DateTime('NOW')
+                        'publishedDate' => new \DateTime('NOW'),
+                        'id'=>4
                     ],
                     5 => [
                         'title' => 'JCVD',
                         'description' => 'Eric',
                         'color' => 'blue',
-                        'published' => false,
+                        'published' => true,
                         'image' => 'https://www.programme.tv/imgre/fit/~1~tel~2021~07~12~6801abcb-554d-4a8b-8667-d05cb7f88734.png/1160x500/crop-from/top/quality/80/focus-point/1783,871/jean-claude-van-damme-devoile-les-causes-de-ses-problemes-de-drogue.jpg',
-                        'publishedDate' => new \DateTime('NOW')
+                        'publishedDate' => new \DateTime('NOW'),
+                        'id'=>5
                     ]
                 ];
 
